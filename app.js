a9c9dbb34c6527c51e010706561ae8c1ab81fa37
Vaibhav kakkar <71547174+vaibhavkakkar15@users.noreply.github.com>
2022-11-17 10:15:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// var scrollpos = window.scrollY; // window scroll position
+// var wh = window.innerHeight - 50; // as soon as element touches bottom with offset event starts
+// var element = document.querySelector("#cart"); //element
+
+// window.addEventListener("scroll", function () {
+//   if (scrollpos > element.offsetTop - wh) {
+//     element.classList.add("onScroll");
+//   }
+// });
